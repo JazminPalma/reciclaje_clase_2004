9851766e1b0f1ae347612282accf5a370b1d5a31
JazminPalma <jazminpalmasantos0@gmail.com>
2026-06-23 10:59:58 -0600

    Update title in app.py to reflect new author

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import tensorflow  as tf
 from PIL import Image
 
 st.set_page_config(page_title="Reciclaje IA-ISC", layout="centered")
-st.title("Modelo predictivo de reciclaje clase de IA-ISC-campus Comayagua-2026-Anny-Gutierrez")
+st.title("Modelo predictivo de reciclaje clase de IA-ISC-campus Comayagua-2026-Jeimy Palma")
 st.write("Suba una imagen para clasificar con el modelo MobileNetV2 pre-entrenado")
 
 IMG_SIZE= (224,224)
