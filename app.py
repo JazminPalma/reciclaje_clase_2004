c6aa631e73966b57e144168863e6d86a30f54ae4
JazminPalma <jazminpalmasantos0@gmail.com>
2026-06-23 10:28:42 -0600

    Fix typo in set_page_config parameters

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ import streamlit as st
 import tensorflow as tf
 from PIL import Image
 
-st.set_page_config(page_tittle="Reciclaje IA-  ISC". layout="centered")
+st.set_page_config(page_tittle="Reciclaje IA-  ISC", layout="centered")
 st.tittle("Modelo predictivo Reciclaje Clase de IA-ISC Campus-Comayagua 2026")
 st.write("Suba una imagen para clasificar con el modelo MobileNetV2 pre entrenado")
